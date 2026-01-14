094473441e5e097e1f6b885c62c4df9556298438
Nishan Paudel <your_email@example.com>
2026-01-14 18:55:41 +0545

    Add templates, update views and URLs, fix requirements file

--- bms/pustak/views.py
+++ bms/pustak/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def books(request):
+    # return HttpResponse("Welcome to django")
+    return render(request, 'books.html')

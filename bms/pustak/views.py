f2f4b1d6723ea3663d35f7b9b86579de4cc75286
Nishan Paudel <your_email@example.com>
2026-01-18 17:35:53 +0545

    Update views, urls and fix gitignore

--- bms/pustak/views.py
+++ bms/pustak/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
 def books(request):
     # return HttpResponse("Welcome to django")
-    return render(request, 'books.html')
+    if request.method == "POST": 
+        bn = request.POST.get('book_name')
+        bd = request.POST.get('book_description')
+        bi = request.FILES.get('book_image')
+        Book.objects.create(book_name = bn, book_description = bd, book_image = bi)
+        return redirect('/')
+    queryset = Book.objects.all()
+    context = {'books': queryset}
+
+    return render(request, 'pustak/books.html', context)
